2d993016c3bd03ab70509183b79e1783d6f899f4
matheusryan25 <mattsantos26@hotmail.com>
2025-04-14 13:44:42 -0400

    Update Policy.java
    Added static field to track number of Policy objects

--- Policy.java
+++ Policy.java
@@ -3,26 +3,50 @@ public class Policy {
     private String providerName;
     private PolicyHolder policyHolder;
 
+    private static int policyCount = 0;
+
     public Policy(int num, String provider, PolicyHolder holder) {
         policyNumber = num;
         providerName = provider;
         policyHolder = new PolicyHolder(holder);
+        policyCount++;
     }
 
-    public int getPolicyNumber() { return policyNumber; }
-    public String getProviderName() { return providerName; }
-    public PolicyHolder getPolicyHolder() { return new PolicyHolder(policyHolder); }
+    public int getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public String getProviderName() {
+        return providerName;
+    }
+    
+    public PolicyHolder getPolicyHolder() {
+        return new PolicyHolder(policyHolder);
+    }
 
     public double getPolicyPrice() {
         double baseFee = 600;
         double bmi = policyHolder.getBMI();
 
-        if (bmi > 35)
+        if (bmi > 35) {
             baseFee += 75;
+        }
 
-        if (policyHolder.getSmokingStatus().equalsIgnoreCase("smoker"))
+        if (policyHolder.getSmokingStatus().equalsIgnoreCase("smoker")) {
             baseFee += 100;
+        }
 
         return baseFee;
+    }
+
+    public static int getPolicyCount() {
+        return policyCount;
+    }
+
+    public String toString() {
+        return "Policy Number: " + policyNumber +
+               "\nProvider Name: " + providerName + "\n" +
+               policyHolder.toString() +
+               String.format("\nPolicy Price: $%.2f\n", getPolicyPrice());
     }
 }
